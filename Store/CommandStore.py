fb2005e5e25d6e2cc14c6ce966778733787c6c80
Artyom Akzhigitov <hexerpowers@gmail.com>
2023-04-08 11:57:34 +0300

    Фикс репозитория

--- Store/CommandStore.py
+++
@@ -1,9 +0,0 @@
-class CommandStore:
-    def __init__(self, config, lgm):
-        self.store = []
-
-    def get_full_val(self):
-        return self.store
-
-    def set_full_val(self, value):
-        self.store = value
